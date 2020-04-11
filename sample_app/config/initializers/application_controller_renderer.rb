94540217a1345851a5ea7ec4bb4a3429b5d4ccb7
imcoco18 <coco.jasmine18@icloud.com>
2020-04-11 20:46:00 +0900

    rails 1周目

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '053b05c5176fdfccea8054f66f73cc7eeee0e0c31f6e3f648171b37cb688ace8b4286e86794dc4e062d9dd331f60bc87b72c61b75f08b30d2e1f6e8bb4f3f336'
