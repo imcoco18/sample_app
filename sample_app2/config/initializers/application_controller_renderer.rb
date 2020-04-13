c1d0aab0fe4602f7187d7f284fd2deafb355a4b2
imcoco18 <coco.jasmine18@icloud.com>
2020-04-13 13:20:42 +0900

    [Finish]3-8 second

--- sample_app2/config/initializers/application_controller_renderer.rb
+++ sample_app2/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ea4faec94ad2575042e11269e75b34fea276d7e9a2e882c23f61b825a686f9d4bf4d4622ddac2c68a931ee31d6e3119e77688d5961415511c0995d1abdad9c37'
