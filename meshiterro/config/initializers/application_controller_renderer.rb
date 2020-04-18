b9df34d52852a7db1e626c58fc7cf80512f9fe07
imcoco18 <coco.jasmine18@icloud.com>
2020-04-18 22:56:05 +0900

    [Finish]5-6

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = 'b08541a68c336a0d18a7874774eace56518cfc2d49dd9330a6e6e58de7bf61540191dba5e12b738c053a12f607cd1cb2b1c3565afa5279487f9f181c2d05b87d'
